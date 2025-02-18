afb2a3730f395abd8df83b9c160fa13d24295fa8
minseong99 <162446292+minseong99@users.noreply.github.com>
2025-02-18 14:35:34 +0900

    패키지 설치

--- EnglishStudyingAppFront/src/services/api.ts
+++ EnglishStudyingAppFront/src/services/api.ts
@@ -2,7 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // NestJS 서버 주소 (개발시)
+  // 백엔드가 로컬 PC에서 실행 중이면 -> http://192.168.124.100:3000
+  // NestJS 서버 주소 (개발 시) -> http://localhost:3000
+  baseURL: 'http://192.168.124.100:3000', // NestJS 서버 주소 (개발시)
   timeout: 5000,
 });
 
