5ade1e6c936e5e8e98eda09b3d81f4f8a28776f1
minseong99 <162446292+minseong99@users.noreply.github.com>
2025-02-20 12:04:05 +0900

    메인 화면 추가 및 채팅 기능 추가
    메인화면을 추가하여 음성, 채팅으로 선택하여 대화 할 수 있도록 서비스를 확장하였다.

--- EnglishStudyingAppFront/src/App.tsx
+++ EnglishStudyingAppFront/src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import MainMenuScreen from './components/MainMenuScreen';
 import SpeakerSelectionScreen from './components/SpeakerSelectionScreen';
 import ChatVoiceScreen from './components/ChatVoiceScreen';
 import ChatScreen from './components/ChatScreen';
@@ -10,14 +11,14 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SpeakerSelection">
+      <Stack.Navigator initialRouteName="MainMenu">
+        <Stack.Screen name="MainMenu" component={MainMenuScreen} options={{ title: "메인 메뉴" }} />
         <Stack.Screen name="SpeakerSelection" component={SpeakerSelectionScreen} options={{ title: "화자 선택" }} />
         <Stack.Screen name="ChatVoice" component={ChatVoiceScreen} options={{ title: "음성 대화" }} />
+        <Stack.Screen name="ChatText" component={ChatScreen} options={{ title: "텍스트 대화" }} />
       </Stack.Navigator>
     </NavigationContainer>
-
   );
 };
 
